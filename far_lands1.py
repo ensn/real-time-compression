8ce6212beec3cb9284831ca41ac0b12d4d99c2c7
Daniel D <66746948+ensn@users.noreply.github.com>
2022-08-05 13:25:32 +0200

    Add files via upload

---
+++ far_lands1.py
@@ -0,0 +1,67 @@
+import datetime, os, time, keyboard
+
+#p toggels partial (1s; 150 t/min)
+#o toggles full auto (1day; 15 sec/t)
+#r extra boost (5 sec/use)
+
+def glitch(sec):
+    print((datetime.datetime.today()+datetime.timedelta(seconds=sec)).strftime('time %H:%M:%S'))
+    time.sleep(0.07)
+    print((datetime.datetime.today()-datetime.timedelta(seconds=sec)).strftime('time %H:%M:%S'))
+
+def glitchday():
+    print((datetime.date.today()+datetime.timedelta(days=1)).strftime('date %d-%m-%Y'))
+    time.sleep(0.07)
+    print((datetime.date.today()-datetime.timedelta(days=1)).strftime('date %d-%m-%Y'))
+    
+
+class Controller():
+    def __init__(this):
+        this.running = 0
+        this.start_time=0
+        this.wait_for_manual = 1
+    def periodic_call(this):
+        if this.running == 0:
+            if keyboard.is_pressed("o"):
+                glitchday()
+                time.sleep(0.5)
+                this.start_time=time.perf_counter()-0.5
+                this.running = 1
+            if keyboard.is_pressed("p"):
+                glitch(1)
+                time.sleep(0.5)
+                this.start_time=time.perf_counter()-0.5
+                this.running=2
+                
+        elif this.running == 1:
+            if keyboard.is_pressed("o"):
+                time.sleep(0.5)
+                this.running=0
+            elif time.perf_counter()>this.start_time+15:
+                glitchday()
+                this.start_time=time.perf_counter()
+        
+        else:
+            if keyboard.is_pressed("p"):
+                time.sleep(0.5)
+                this.running=0
+            elif time.perf_counter()>this.start_time+(60/150):
+                glitch(1)
+                this.start_time=time.perf_counter()
+
+        
+        if this.wait_for_manual ==1:
+            if keyboard.is_pressed("r"):
+                glitch(5)
+                this.wait_for_manual = 0
+        else:
+            if not keyboard.is_pressed("r"):
+                this.wait_for_manual = 1
+            
+
+   
+time.sleep(0)
+c=Controller()
+while True:
+    c.periodic_call()
+    time.sleep(0.01)
